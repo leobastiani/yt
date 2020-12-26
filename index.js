e4922158a716c012a9cd8d2bf2b1b903a53727aa
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-12-26 13:38:51 -0300

    Formatted

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = (argv = process.argv) => {
-  const execa = require('execa');
-  const fs = require('fs');
-  const path = require('path');
-  const { docopt } = require('docopt');
+  const execa = require("execa");
+  const fs = require("fs");
+  const path = require("path");
+  const { docopt } = require("docopt");
   const execname = path.basename(argv[1]);
   doc = `Usage:
   ${execname} [options] [<urls> ...]
@@ -15,39 +15,39 @@ Options:
   -e --ext=<e>         Video extension [default: mp4].
 `;
   const arguments = docopt(doc, { argv: argv.slice(2) });
-  const DEBUG = arguments['--debug'];
+  const DEBUG = arguments["--debug"];
   const debug = DEBUG ? console.debug : () => {};
   debug({ arguments });
 
-  const urls = arguments['<urls>'].length
-    ? arguments['<urls>']
+  const urls = arguments["<urls>"].length
+    ? arguments["<urls>"]
     : (() => {
-      const clipboardy = require('clipboardy');
-      return clipboardy.readSync().trim().split(/\s+/gm).filter(Boolean);
-    })();
+        const clipboardy = require("clipboardy");
+        return clipboardy.readSync().trim().split(/\s+/gm).filter(Boolean);
+      })();
 
-  if(urls.length == 0) {
-    docopt(doc, { argv: ['-h'] });
+  if (urls.length == 0) {
+    docopt(doc, { argv: ["-h"] });
   }
 
-  if(process.cwd() == process.env.USERPROFILE) {
+  if (process.cwd() == process.env.USERPROFILE) {
     process.chdir(`${process.env.USERPROFILE}\\Desktop`);
   }
 
-  const resolution = parseInt(arguments['--resolution']);
-  const ext = arguments['--ext'];
-  for(const url of urls) {
+  const resolution = parseInt(arguments["--resolution"]);
+  const ext = arguments["--ext"];
+  for (const url of urls) {
     execa.sync(
       `youtube-dl`,
       [
-        arguments['--audio-only']
-          ? '--extract-audio --audio-format mp3'
+        arguments["--audio-only"]
+          ? "--extract-audio --audio-format mp3"
           : `-f "best[height<=${resolution}][ext=${ext}]"`,
         "--output",
         `"%(title)s.%(ext)s"`,
         `"${url}"`,
       ],
-      { shell: true, stdio: ['inherit', 'inherit', 'inherit'] }
+      { shell: true, stdio: ["inherit", "inherit", "inherit"] }
     );
   }
 };
